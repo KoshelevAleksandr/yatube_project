6b8fcd2a659d48d45717c7a085cd0a18fe4a9046
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2022-07-11 16:52:48 +0300

    Настроен кабинет администратитора

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,8 +7,8 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
-    pud_date = models.DateTimeField(auto_now_add=True)
-    autor = models.ForeignKey(
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts'
