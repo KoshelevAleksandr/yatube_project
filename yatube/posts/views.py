e6c49f56c26d153f502cf526407425a7c69f7981
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2022-07-11 23:11:51 +0300

    Подстроена главная страница, добавлена БД

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def index(request):
     templates = 'posts/index.html'
     title = 'Главная страница'
     text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
         'text': text,
+        'posts': posts,
     }
     return render(request, templates, context)
 
